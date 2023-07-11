2b0b2eb6266e0eef6ac22f91e7db31d5d6a3afda
vk196 <victorumeh196@gmail.com>
2023-07-11 02:05:01 +0100

    modfied code and implemented isFull, isEmpty, and pop methods

--- stack-implementation/index.js
+++ stack-implementation/index.js
@@ -1,30 +1,47 @@
 "use strict";
 class Stack {
-  constructor() {
+  constructor(size) {
     this.struct = [];
     this.base = -1;
+    this.size = size;
   }
   // push
   push(data) {
+    if (this.base === this.size - 1) {
+      return console.error("stack is full");
+    }
     this.base++;
     this.struct[this.base] = data;
+    return this.struct;
   }
   // pop
-  pop(){
-    if(base === -1){
-        
+  pop() {
+    if (this.base === -1) {
+      return console.log("stack is empty");
     }
+    const lastIndex = this.struct.length - 1;
+    const lastItem = this.struct[lastIndex];
+    this.struct.length = lastIndex;
+    return lastItem;
   }
   // isFull
+  isFull() {
+    return this.size - 1 === this.base;
+  }
   // isEmpty
+  isEmpty() {
+    return this.base === -1;
+  }
   // clear
   // peek
   // search
 }
 
-const test = new Stack();
+const test = new Stack(3);
 test.push(1);
 test.push(2);
 test.push(3);
-test.push(4);
-console.log(test);
+console.log(test.push(4));
+console.log(test.isFull());
+console.log(test.isEmpty());
+// console.log(test);
